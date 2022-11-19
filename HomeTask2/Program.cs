e2cdc93a348da42be487b04bbed11935418c5ef5
Ilya Besedin <besedka07@icloud.com>
2022-11-19 18:00:00 +0300

    HomeWork1

---
+++ HomeTask2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает
+//  два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+Console.WriteLine("Введите два числа:");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b) Console.WriteLine($"а={a}; b={b}; -> max={a}");
+else if (a < b) Console.WriteLine($"а={a}; b={b}; -> max={b}");
+else Console.WriteLine("Эти числа равны");
---
+++ HomeTask8/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+Console.WriteLine("Введите число N:");
+int count = 1;
+int n = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+Console.Write($"{n} -> ");
+while (count <= n)
+{
+    if (count % 2 == 0) Console.Write($"{count} ");
+    count++;
+}
